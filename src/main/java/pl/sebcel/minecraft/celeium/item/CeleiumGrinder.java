38d5d52cddddc1e9e9c28812b6948266b6018dc6
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2016-09-27 21:36:29 +0200

    Celeium Grinder now can turn dirt into cobblestone

---
+++ src/main/java/pl/sebcel/minecraft/celeium/item/CeleiumGrinder.java
@@ -0,0 +1,46 @@
+package pl.sebcel.minecraft.celeium.item;
+
+import pl.sebcel.minecraft.celeium.CeleiumMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockDirt;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class CeleiumGrinder extends ItemBase {
+
+    public CeleiumGrinder(String name) {
+        super(name);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        
+        IBlockState iblockstate = worldIn.getBlockState(pos);
+        Block block = iblockstate.getBlock();
+
+        if (block == Blocks.GRASS || block == Blocks.GRASS_PATH || block == Blocks.DIRT)
+        {
+            if (!worldIn.isRemote)
+            {
+                worldIn.setBlockState(pos, Blocks.COBBLESTONE.getDefaultState(), 11);
+                stack.damageItem(1, playerIn);
+            }
+            
+            return EnumActionResult.SUCCESS;
+        }
+        
+        return EnumActionResult.PASS;
+    }
+}
