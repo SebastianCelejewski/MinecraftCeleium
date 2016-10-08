5b4e03e098fb72476de28c0d8618e67cdfdff587
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2016-10-08 15:45:44 +0200

    Better bounding box and new texture

--- src/main/java/pl/sebcel/minecraft/celeium/block/BlockLamp.java
+++ src/main/java/pl/sebcel/minecraft/celeium/block/BlockLamp.java
@@ -31,19 +31,19 @@ public class BlockLamp extends BlockDirectional {
     
     public static final PropertyBool POWERED = PropertyBool.create("powered");
 
-    protected static final AxisAlignedBB AABB_DOWN_ON = new AxisAlignedBB(0.3125D, 0.875D, 0.375D, 0.6875D, 1.0D, 0.625D);
-    protected static final AxisAlignedBB AABB_UP_ON = new AxisAlignedBB(0.3125D, 0.0D, 0.375D, 0.6875D, 0.125D, 0.625D);
-    protected static final AxisAlignedBB AABB_NORTH_ON = new AxisAlignedBB(0.3125D, 0.375D, 0.875D, 0.6875D, 0.625D, 1.0D);
-    protected static final AxisAlignedBB AABB_SOUTH_ON = new AxisAlignedBB(0.3125D, 0.375D, 0.0D, 0.6875D, 0.625D, 0.125D);
-    protected static final AxisAlignedBB AABB_WEST_ON = new AxisAlignedBB(0.875D, 0.375D, 0.3125D, 1.0D, 0.625D, 0.6875D);
-    protected static final AxisAlignedBB AABB_EAST_ON = new AxisAlignedBB(0.0D, 0.375D, 0.3125D, 0.125D, 0.625D, 0.6875D);
+    protected static final AxisAlignedBB AABB_DOWN_ON = new AxisAlignedBB(0.25D, 0.9D, 0.25D, 0.75D, 1.0D, 0.75D);
+    protected static final AxisAlignedBB AABB_UP_ON = new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 0.1D, 0.75D);
+    protected static final AxisAlignedBB AABB_NORTH_ON = new AxisAlignedBB(0.25D, 0.25D, 0.9D, 0.75D, 0.75D, 1.0D);
+    protected static final AxisAlignedBB AABB_SOUTH_ON = new AxisAlignedBB(0.25D, 0.25D, 0.0D, 0.75D, 0.75D, 0.1D);
+    protected static final AxisAlignedBB AABB_WEST_ON = new AxisAlignedBB(0.9D, 0.25D, 0.25D, 1.0D, 0.75, 0.75D);
+    protected static final AxisAlignedBB AABB_EAST_ON = new AxisAlignedBB(0.0D, 0.25D, 0.25D, 0.1D, 0.75, 0.75D);
     
-    protected static final AxisAlignedBB AABB_DOWN_OFF = new AxisAlignedBB(0.3125D, 0.875D, 0.375D, 0.6875D, 1.0D, 0.625D);
-    protected static final AxisAlignedBB AABB_UP_OFF = new AxisAlignedBB(0.3125D, 0.0D, 0.375D, 0.6875D, 0.125D, 0.625D);
-    protected static final AxisAlignedBB AABB_NORTH_OFF = new AxisAlignedBB(0.3125D, 0.375D, 0.875D, 0.6875D, 0.625D, 1.0D);
-    protected static final AxisAlignedBB AABB_SOUTH_OFF = new AxisAlignedBB(0.3125D, 0.375D, 0.0D, 0.6875D, 0.625D, 0.125D);
-    protected static final AxisAlignedBB AABB_WEST_OFF = new AxisAlignedBB(0.875D, 0.375D, 0.3125D, 1.0D, 0.625D, 0.6875D);
-    protected static final AxisAlignedBB AABB_EAST_OFF = new AxisAlignedBB(0.0D, 0.375D, 0.3125D, 0.125D, 0.625D, 0.6875D);
+    protected static final AxisAlignedBB AABB_DOWN_OFF = new AxisAlignedBB(0.25D, 0.9D, 0.25D, 0.75D, 1.0D, 0.75D);
+    protected static final AxisAlignedBB AABB_UP_OFF = new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 0.1D, 0.75D);
+    protected static final AxisAlignedBB AABB_NORTH_OFF = new AxisAlignedBB(0.25D, 0.25D, 0.9D, 0.75D, 0.75D, 1.0D);
+    protected static final AxisAlignedBB AABB_SOUTH_OFF = new AxisAlignedBB(0.25D, 0.25D, 0.0D, 0.75D, 0.75D, 0.1D);
+    protected static final AxisAlignedBB AABB_WEST_OFF = new AxisAlignedBB(0.9D, 0.25D, 0.25D, 1.0D, 0.75, 0.75D);
+    protected static final AxisAlignedBB AABB_EAST_OFF = new AxisAlignedBB(0.0D, 0.25D, 0.25D, 0.1D, 0.75, 0.75D);
     
     protected BlockLamp()
     {
